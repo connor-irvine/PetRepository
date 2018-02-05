476b55629fade806772a87b45e38cd952ee9b00f
Connor Irvine <connor.irvine@hmrc.digital.gov.uk>
2018-02-05 16:11:25 +0000

    add the end of days work

--- src/main/scala/PetApp.scala
+++ src/main/scala/PetApp.scala
@@ -6,5 +6,10 @@ class PetApp {
   private val Pets = new ArrayBuffer[PetApp]()
   def all() : List[PetApp] = Nil
   def findByName(name : String) : Option[PetApp] = None
-
+  def dogs() : List[PetApp] = Nil
+  def cats() : List[PetApp] = Nil
+  def other() : List[PetApp] = Nil
+  def add(petApp: PetApp) : List[PetApp]
+  def removeByName(name : String) : List[PetApp]
+  def update(petApp: PetApp) : List[PetApp]
 }
