e4f727240e7640cdcbfa73869d013b49ef2dbb9e
Connor Irvine <connor.irvine@hmrc.digital.gov.uk>
2018-02-14 14:04:36 +0000

    add code with closing brackets corrected

--- src/main/scala/repositories/PetRepository.scala
+++ src/main/scala/repositories/PetRepository.scala
@@ -6,40 +6,42 @@ import scala.collection.mutable.ArrayBuffer
 
 object PetRepository {
 
-  private val _pet : ArrayBuffer[Pet] = new ArrayBuffer[Pet]()
+  private val _pet: ArrayBuffer[Pet] = new ArrayBuffer[Pet]()
 
-  def all() : List[Pet] = _pet.toList
+  def all(): List[Pet] = _pet.toList
 
-  def findByName(name : String) : Option[Pet] = { None
+  def findByName(name: String): Option[Pet] = {
+    None
     _pet.find(pet => pet.name == name)
   }
 
-  def dogs() : List[Dog] = {
- _pet.toList.flatMap {
-  case d @ Dog(_)  =>
-    Some (d)
-  case _ =>  None
-}
-  }
-
-  def cats() :	List[Cat]	=	{
-    _pet.toList.flatMap	{
-      case	c	@	Cat(_)	=>
-        Some(c)
-      case	_	=>	None
+  def dogs(): List[Dog] =
+    _pet.toList.flatMap {
+      case d@Dog(_) =>
+        Some(d)
+      case _ => None
     }
-  }
 
-  def add(pet: Pet*) : List[Pet] = {
+
+  def cats(): List[Cat] =
+    _pet.toList.flatMap {
+      case c@Cat(_) =>
+        Some(c)
+      case _ => None
+    }
+
+
+  def add(pet: Pet*): List[Pet] = {
     pet.foreach(p => _pet.+=(p))
     all()
   }
 
-  def removeByName(name : String) : List[Pet] = {
-  all().filterNot(p => p.name == name)
+  def removeByName(name: String): List[Pet] = {
+    all().filterNot(p => p.name == name)
   }
 
 
-  def update(petApp: Pet) : List[Pet] = Nil
+  def update(petApp: Pet): List[Pet] = Nil
 
 
+}
